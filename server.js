9cffbda414fef7beac0f08ba5793784d0158b258
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-28 15:24:10 +0100

    Installed and added helmet.js to code

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const fs = require('fs');
 const https = require('https');
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
     return res.send('Your personal secret value is 42!');
@@ -15,8 +19,8 @@ app.get('/', (req, res) => {
 });
 
 https.createServer({
-    key: '',
-    cert: '',
-}).listen(PORT, () => {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
