d457d4bf7fb10be352e54fc307b3dafc82b2bead
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-28 16:11:15 +0100

    Added more code

--- server.js
+++ server.js
@@ -3,12 +3,35 @@ const https = require('https');
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+const passport = require('passport');
+const {Strategy} = require('passport-google-oauth20');
+
+require('dotenv').config();
 
 const PORT = 3000;
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+    console.log('Google profile', profile);
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
     const isLoggedIn = true; //TODO
